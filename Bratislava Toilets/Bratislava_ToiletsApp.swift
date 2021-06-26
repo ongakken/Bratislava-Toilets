651ea0e8b14cc64a55eaa91a12fadb729c7e31ad
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2021-06-26 21:43:51 +0200

    Initial Commit

---
+++ Bratislava Toilets/Bratislava_ToiletsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bratislava_ToiletsApp.swift
+//  Bratislava Toilets
+//
+//  Created by Simon Slamka on 26.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Bratislava_ToiletsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
