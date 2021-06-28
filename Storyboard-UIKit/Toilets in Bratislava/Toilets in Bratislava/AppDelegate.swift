2510e4ba155556c5c5da76e78692d11be4a64919
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2021-06-28 14:51:28 +0200

    implemented AdMob and configured it to use a LIVE banner; built successfully, but not tested yet;

--- Storyboard-UIKit/Toilets in Bratislava/Toilets in Bratislava/AppDelegate.swift
+++ Storyboard-UIKit/Toilets in Bratislava/Toilets in Bratislava/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import GoogleMaps
+import GoogleMobileAds
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GMSServices.provideAPIKey("AIzaSyBq_ofuAcHghFQsqmOnkm8EviXwRBPUf24")
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
         return true
     }
 
--- Storyboard-UIKit/Toilets in Bratislava/Toilets in Bratislava/ViewController.swift
+++ Storyboard-UIKit/Toilets in Bratislava/Toilets in Bratislava/ViewController.swift
@@ -7,37 +7,66 @@
 
 import UIKit
 import GoogleMaps
+import GoogleMobileAds
 
-class ViewController: UIViewController, GMSMapViewDelegate {
+class ViewController: UIViewController, GMSMapViewDelegate, GADBannerViewDelegate {
+	
+	var bannerView: GADBannerView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+		bannerView = GADBannerView(adSize: kGADAdSizeBanner)
         let camera = GMSCameraPosition.camera(withLatitude: 48.141, longitude: 17.109, zoom: 13.5)
         let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
         mapView.delegate = self
-        mapView.settings.myLocationButton = true
         self.view = mapView
+		addBannerViewToView(bannerView)
+		bannerView.adUnitID = "ca-app-pub-9086446979210331/7085643056"
+		bannerView.rootViewController = self
+		bannerView.delegate = self
+		bannerView.load(GADRequest())
         //self.view.addSubview(mapView)
         
-        // verejne hajzle namestie Eugena Suchona
+        // verejne WC namestie Eugena Suchona
         let marker_suchon = GMSMarker()
         marker_suchon.position = CLLocationCoordinate2D(latitude: 48.141, longitude: 17.109)
         marker_suchon.title = "Eugen Suchon Square"
         marker_suchon.snippet = "The Old Town"
+        marker_suchon.icon = GMSMarker.markerImage(with: .yellow)
         marker_suchon.map = mapView
         
-        // verejne hajzle Eurovea
+        // verejne WC Eurovea
         let marker_eurovea = GMSMarker()
         marker_eurovea.position = CLLocationCoordinate2D(latitude: 48.140, longitude: 17.121)
         marker_eurovea.title = "Eurovea Shopping Center"
         marker_eurovea.snippet = "The Old Town"
-        marker_eurovea.icon = GMSMarker.markerImage(with: .blue)
+        marker_eurovea.icon = GMSMarker.markerImage(with: .yellow)
         marker_eurovea.map = mapView
     }
     
     // MARK: GMSMapViewDelegate
     
     func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
-        UIApplication.shared.open(URL(string: "https://www.google.com/maps/dir/\(marker.position.latitude),\(marker.position.longitude),16z")!)
+        UIApplication.shared.open(URL(string: "https://www.google.com/maps/dir/?api=1&origin=&destination=\(marker.position.latitude),\(marker.position.longitude)&travelmode=walking")!)
     }
+	
+	func addBannerViewToView(_ bannerView: GADBannerView) {
+		bannerView.translatesAutoresizingMaskIntoConstraints = false
+		view.addSubview(bannerView)
+		view.addConstraints([NSLayoutConstraint(item: bannerView,
+												attribute: .bottom,
+												relatedBy: .equal,
+												toItem: view.safeAreaLayoutGuide,
+												attribute: .top,
+												multiplier: 1,
+												constant: 0),
+							 NSLayoutConstraint(item: bannerView,
+												attribute: .centerX,
+												relatedBy: .equal,
+												toItem: view,
+												attribute: .centerX,
+												multiplier: 1,
+												constant: 0)
+		])
+	}
 }
